0bad16931c3c5b9606e4fd5ab62119943d5e687a
Lucas Gaviao <143828910+LucasGaviao@users.noreply.github.com>
2024-05-07 23:00:17 -0300

    Update and rename r7.c to Recursao-7.c

--- Recursao-7.c
+++ Recursao-7.c
@@ -1,3 +1,5 @@
+/*Crie um programa em C que receba um vetor de números reais com 100 elementos.
+Escreva uma função recursiva que inverta ordem dos elementos presentes no vetor*/
 #include <stdio.h>
 
 void inveterVetor100(int *vet, int t){
